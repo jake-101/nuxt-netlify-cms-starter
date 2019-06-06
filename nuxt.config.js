64bc29876851d465ebf3ecc00838e724ceb58bae
jake <jake101@gmail.com>
2019-06-06 10:32:32 -0700

    moved identity.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    script: [{ src: './assets/identity' }],
+    script: [{ src: '/identity.js' }],
     title: 'Norman Bertolino',
     meta: [
       {
