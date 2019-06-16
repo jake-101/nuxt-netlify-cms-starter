77ef01aafc9180ca39cffafbea4fe6ce9b7b8e40
jake <jake101@gmail.com>
2019-06-16 00:12:57 -0700

    confirm page

---
+++ pages/confirm.vue
@@ -0,0 +1,111 @@
+<template>
+  <section>
+    <Container>
+      <div class="intro">
+        <h1>Sent</h1>
+      </div>
+    </Container>
+      
+  </section>
+</template>
+
+<script>
+import ArticleCard from '~/components/ArticleCard'
+import Container from '~/components/Container'
+import blogs from '~/content/works.json'
+import pages from '~/content/pages.json'
+var tinycolor = require("tinycolor2");
+
+export default {
+
+  components: {
+    ArticleCard,
+    Container
+  },
+data: function () {
+    return {
+      colors: null
+    }
+  },
+  async asyncData({ app }) {
+    async function awaitImport(blog) {
+      const wholeMD = await import(`~/content/work/${blog.slug}.md`)
+      return {
+        attributes: wholeMD.attributes,
+        link: blog.slug
+      }
+    }
+
+
+
+    const blogList = await Promise.all(
+      blogs.map(blog => awaitImport(blog))
+    ).then(res => {
+      return {
+        blogList: res
+      }
+    })
+
+
+
+    return blogList
+  },
+  created() {
+    this.getColors()
+  },
+  methods: {
+    before() {
+this.$anime.set('.article-card',{opacity:0})
+    },
+    grow(e) {
+        const elm = e.target
+  console.log(elm)
+
+   this.$anime({
+     targets: elm,
+     scale: 1.1,
+     duration: 200
+   })
+    },
+    getColors() {
+      var rand = tinycolor.random();
+      var gray = tinycolor('#111')
+      var colors = tinycolor(rand).analogous();
+              this.colors = colors.map(function(t) { return tinycolor.mix(t,gray).desaturate(20).toHexString(); })
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" >
+.intro {
+  text-align: center;
+  margin-bottom: 2.4rem;
+
+  h1 {
+    margin-top: 0;
+  }
+}
+.container {
+  display: block;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 1rem;
+
+  &.flex {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    @media (max-width: 567px) {
+      display: block;
+    }
+  }
+
+  &.narrow {
+    max-width: 760px;
+  }
+}
+</style>
